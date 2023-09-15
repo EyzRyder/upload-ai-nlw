9212b9a15894fae7c74684620973f1b087629153
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-09-15 01:28:59 -0300

    finished project

---
+++ api/src/server.ts
@@ -0,0 +1,22 @@
+import fastify from "fastify";
+import {fastifyCors} from "@fastify/cors";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+
+const app = fastify();
+const PORT = 3333;
+
+app.register(fastifyCors, {
+  origin:'*',
+});
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
+
+app.listen({
+  port: PORT,
+}).then(() => console.log(`HTTP Server Running!! http://localhost:${PORT}`));
---
+++ web/README.md
@@ -0,0 +1,4 @@
+# Upload-AI Web
+
+Esse é um projeto feito no evento do NLW-AI,
+essa aplicação que possibilita realizar upload de videos e por meio de IA, criar automaticamente títulos chamativos e descrições com um boa indexação.
